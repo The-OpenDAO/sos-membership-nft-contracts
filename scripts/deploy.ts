f82b10fffd234b9ca21acbcdb70533249f2f23c8
Tony Ke <thagki9@outlook.com>
2022-01-23 00:04:07 +0800

    Add mainnet configuration

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,21 +6,23 @@ async function main() {
 
   const merkleRoot = "0xac7bed30976a9e81a333313a62bf9fc12c479b9250fc39f0fa061bf5fc2e0383";
   const endTime = 0;
+  const url = "https://raw.githubusercontent.com/The-OpenDAO/sos-membership-nft-contracts/main/metadata/json/";
 
   const nft = await nftFactory.deploy(
     merkleRoot,
     endTime,
-    "https://raw.githubusercontent.com/The-OpenDAO/sos-membership-nft-contracts/main/metadata/json/");
+    url);
   console.log("Deploy at tx %s", nft.deployTransaction.hash);
 
   await nft.deployed();
   console.log("Deploy to address %s", nft.address);
 
   await run("verify:verify", {
-    address: nft.address,
+    address: "0xd22f83e8A1502B1d41c0b40cF64B291a6eaBC44d",
     constructorArguments: [
       merkleRoot,
       endTime,
+      url,
     ],
   });
 }
